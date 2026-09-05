black-desk/mamba

commit 67f36d4a4815b559db4f3fcda2516f33a88a67d8

Added mirrored channels (#3125)

libmamba/src/api/channel_loader.cpp
@@ -42,6 +42,53 @@ namespace mamba
             }
             return MRepo(pool, prefix_data);
         }
+
+        void create_subdirs(
+            Context& ctx,
+            ChannelContext& channel_context,
+            const specs::Channel& channel,
+            MultiPackageCache& package_caches,
+            std::vector<MSubdirData>& subdirs,
+            std::vector<mamba_error>& error_list,
+            std::vector<std::pair<int, int>>& priorities,
+            int& max_prio,
+            specs::CondaURL& prev_channel_url
+        )
+        {
+            for (const auto& platform : channel.platforms())
+            {
+                auto sdires = MSubdirData::create(
+                    ctx,
+                    channel_context,
+                    channel,
+                    platform,
+                    channel.platform_url(platform).str(specs::CondaURL::Credentials::Show),
+                    package_caches,
+                    "repodata.json"
+                );
+                if (!sdires.has_value())
+                {
+                    error_list.push_back(std::move(sdires).error());
+                    continue;
+                }
+                auto sdir = std::move(sdires).value();
+                subdirs.push_back(std::move(sdir));
+                if (ctx.channel_priority == ChannelPriority::Disabled)
+                {
+                    priorities.push_back(std::make_pair(0, 0));
+                }
+                else
+                {
+                    // Consider 'flexible' and 'strict' the same way
+                    if (channel.url() != prev_channel_url)
+                    {
+                        max_prio--;
+                        prev_channel_url = channel.url();
+                    }
+                    priorities.push_back(std::make_pair(max_prio, 0));
+                }
+            }
+        }
     }
 
     expected_t<void, mamba_aggregated_error>
@@ -59,42 +106,42 @@ namespace mamba
 
         std::vector<mamba_error> error_list;
 
+        for (const auto& mirror : ctx.mirrored_channels)
+        {
+            for (auto channel : pool.channel_context().make_channel(mirror.first, mirror.second))
+            {
+                create_subdirs(
+                    ctx,
+                    pool.channel_context(),
+                    channel,
+                    package_caches,
+                    subdirs,
+                    error_list,
+                    priorities,
+                    max_prio,
+                    prev_channel_url
+                );
+            }
+        }
+
         for (const auto& location : ctx.channels)
         {
-            for (auto channel : pool.channel_context().make_channel(location))
+            // TODO: C++20, replace with contains
+            if (ctx.mirrored_channels.find(location) == ctx.mirrored_channels.end())
             {
-                for (const auto& platform : channel.platforms())
+                for (auto channel : pool.channel_context().make_channel(location))
                 {
-                    auto sdires = MSubdirData::create(
+                    create_subdirs(
                         ctx,
                         pool.channel_context(),
                         channel,
-                        platform,
-                        channel.platform_url(platform).str(specs::CondaURL::Credentials::Show),
                         package_caches,
-                        "repodata.json"
+                        subdirs,
+                        error_list,
+                        priorities,
+                        max_prio,
+                        prev_channel_url
                     );
-                    if (!sdires.has_value())
-                    {
-                        error_list.push_back(std::move(sdires).error());
-                        continue;
-                    }
-                    auto sdir = std::move(sdires).value();
-                    subdirs.push_back(std::move(sdir));
-                    if (ctx.channel_priority == ChannelPriority::Disabled)
-                    {
-                        priorities.push_back(std::make_pair(0, 0));
-                    }
-                    else
-                    {
-                        // Consider 'flexible' and 'strict' the same way
-                        if (channel.url() != prev_channel_url)
-                        {
-                            max_prio--;
-                            prev_channel_url = channel.url();
-                        }
-                        priorities.push_back(std::make_pair(max_prio, 0));
-                    }
                 }
             }
         }

libmamba/include/mamba/core/channel_context.hpp
@@ -53,6 +53,9 @@ namespace mamba
 
         [[nodiscard]] auto make_channel(specs::UnresolvedChannel uc) -> const channel_list&;
         [[nodiscard]] auto make_channel(std::string_view name) -> const channel_list&;
+        [[nodiscard]] auto
+        make_channel(std::string_view name, const std::vector<std::string>& mirrors)
+            -> const channel_list&;
 
         [[nodiscard]] auto params() const -> const specs::ChannelResolveParams&;
 

libmamba/include/mamba/core/context.hpp
@@ -215,6 +215,7 @@ namespace mamba
 #endif
         };
 
+        std::map<std::string, std::vector<std::string>> mirrored_channels;
         std::string channel_alias = "https://conda.anaconda.org";
         specs::AuthenticationDataBase& authentication_info();
         const specs::AuthenticationDataBase& authentication_info() const;

libmamba/include/mamba/specs/channel.hpp
@@ -88,8 +88,14 @@ namespace mamba::specs
         ) -> channel_list;
 
         Channel(CondaURL url, std::string display_name, platform_list platforms = {});
+        Channel(std::vector<CondaURL> mirror_urls, std::string display_name, platform_list platforms = {});
 
         [[nodiscard]] auto is_package() const -> bool;
+
+        [[nodiscard]] auto mirror_urls() const -> const std::vector<CondaURL>&;
+        [[nodiscard]] auto platform_mirror_urls() const -> std::vector<CondaURL>;
+        [[nodiscard]] auto platform_mirror_urls(const std::string_view platform) const
+            -> std::vector<CondaURL>;
 
         [[nodiscard]] auto url() const -> const CondaURL&;
         auto clear_url() -> const CondaURL;
@@ -124,7 +130,9 @@ namespace mamba::specs
 
     private:
 
-        CondaURL m_url;
+        CondaURL platform_url_impl(const CondaURL& url, const std::string_view platform) const;
+
+        std::vector<CondaURL> m_mirror_urls;
         std::string m_display_name;
         util::flat_set<std::string> m_platforms;
     };

libmamba/src/api/configuration.cpp
@@ -1289,6 +1289,16 @@ namespace mamba
                    )
                    .needs({ "default_channels", "target_prefix", "root_prefix" }));
 
+        insert(Configurable("mirrored_channels", &m_context.mirrored_channels)
+                   .group("Channels")
+                   .set_rc_configurable()
+                   .set_env_var_names()
+                   .description("Mirrored channels")
+                   .long_description(  //
+                       "A dictionary where keys are channels names, and values are a list "
+                       "of mirrors urls to use.\n"
+                   ));
+
         insert(Configurable("override_channels_enabled", &m_context.override_channels_enabled)
                    .group("Channels")
                    .set_rc_configurable()

libmamba/src/core/channel_context.cpp
@@ -245,6 +245,29 @@ namespace mamba
         return it->second;
     }
 
+    auto ChannelContext::make_channel(std::string_view name, const std::vector<std::string>& mirrors)
+        -> const channel_list&
+    {
+        if (const auto it = m_channel_cache.find(std::string(name)); it != m_channel_cache.end())
+        {
+            return it->second;
+        }
+
+        std::vector<specs::CondaURL> mirror_urls;
+        mirror_urls.reserve(mirrors.size());
+        for (const auto& mirror : mirrors)
+        {
+            mirror_urls.push_back(specs::CondaURL::parse(mirror));
+        }
+        auto [it, inserted] = m_channel_cache.emplace(
+            name,
+            channel_list{
+                Channel(std::move(mirror_urls), std::string(name), m_channel_params.platforms) }
+        );
+        assert(inserted);
+        return it->second;
+    }
+
     auto ChannelContext::params() const -> const specs::ChannelResolveParams&
     {
         return m_channel_params;

libmamba/src/specs/channel.cpp
@@ -37,33 +37,82 @@ namespace mamba::specs
      *******************************/
 
     Channel::Channel(CondaURL url, std::string display_name, platform_list platforms)
-        : m_url(std::move(url))
+        : Channel(std::vector<CondaURL>(1u, std::move(url)), std::move(display_name), std::move(platforms))
+    //: Channel({ std::move(url) }, std::move(display_name), std::move(platforms))
+    {
+    }
+
+    Channel::Channel(std::vector<CondaURL> mirror_urls, std::string display_name, platform_list platforms)
+        : m_mirror_urls(std::move(mirror_urls))
         , m_display_name(std::move(display_name))
         , m_platforms(std::move(platforms))
     {
-        auto p = m_url.clear_path();
-        p = util::rstrip(p, '/');
-        m_url.set_path(std::move(p), CondaURL::Encode::no);
+        for (auto& url : m_mirror_urls)
+        {
+            auto p = url.clear_path();
+            p = util::rstrip(p, '/');
+            url.set_path(std::move(p), CondaURL::Encode::no);
+        }
     }
 
     auto Channel::is_package() const -> bool
     {
-        return !url().package().empty();
+        return (m_mirror_urls.size() == 1u) && !url().package().empty();
+    }
+
+    auto Channel::mirror_urls() const -> const std::vector<CondaURL>&
+    {
+        return m_mirror_urls;
+    }
+
+    auto Channel::platform_mirror_urls() const -> std::vector<CondaURL>
+    {
+        if (is_package())
+        {
+            return { url() };
+        }
+
+        auto out = std::vector<CondaURL>();
+        out.reserve(mirror_urls().size() * platforms().size());
+        for (const auto& url : mirror_urls())
+        {
+            for (const auto& platform : platforms())
+            {
+                out.push_back(platform_url_impl(url, platform));
+            }
+        }
+        return out;
+    }
+
+    auto Channel::platform_mirror_urls(const std::string_view platform) const -> std::vector<CondaURL>
+    {
+        if (is_package())
+        {
+            return { url() };
+        }
+
+        auto out = std::vector<CondaURL>();
+        out.reserve(mirror_urls().size());
+        for (const auto& url : mirror_urls())
+        {
+            out.push_back(platform_url_impl(url, platform));
+        }
+        return out;
     }
 
     auto Channel::url() const -> const CondaURL&
     {
-        return m_url;
+        return m_mirror_urls.front();
     }
 
     auto Channel::clear_url() -> const CondaURL
     {
-        return std::exchange(m_url, {});
+        return std::exchange(m_mirror_urls.front(), {});
     }
 
     void Channel::set_url(CondaURL url)
     {
-        m_url = std::move(url);
+        m_mirror_urls.front() = std::move(url);
     }
 
     auto Channel::platform_urls() const -> std::vector<CondaURL>
@@ -77,7 +126,7 @@ namespace mamba::specs
         out.reserve(platforms().size());
         for (const auto& platform : platforms())
         {
-            out.push_back(platform_url(platform));
+            out.push_back(platform_url_impl(url(), platform));
         }
         return out;
     }
@@ -88,7 +137,7 @@ namespace mamba::specs
         {
             return url();
         }
-        return (url() / platform);
+        return platform_url_impl(url(), platform);
     }
 
     auto Channel::platforms() const -> const platform_list&
@@ -176,6 +225,11 @@ namespace mamba::specs
             return platforms().contains(plat) ? Match::Full : Match::InOtherPlatform;
         }
         return Match::No;
+    }
+
+    CondaURL Channel::platform_url_impl(const CondaURL& url, const std::string_view platform) const
+    {
+        return { url / platform };
     }
 
     /****************************************

libmamba/tests/src/core/test_configuration.cpp
@@ -519,6 +519,25 @@ namespace mamba
                 util::unset_env("MAMBA_CHANNEL_ALIAS");
             }
 
+            TEST_CASE_FIXTURE(Configuration, "mirrored_channels")
+            {
+                std::string rc1 = unindent(R"(
+                    mirrored_channels:
+                      conda-forge: [https://conda.anaconda.org/conda-forge, https://repo.mamba.pm/conda-forge]
+                      channel1: [https://conda.anaconda.org/channel1]
+                )");
+
+                load_test_config(rc1);
+
+                CHECK_EQ(config.dump(), unindent(R"(
+                          mirrored_channels:
+                            channel1:
+                              - https://conda.anaconda.org/channel1
+                            conda-forge:
+                              - https://conda.anaconda.org/conda-forge
+                              - https://repo.mamba.pm/conda-forge)"));
+            }
+
             TEST_CASE_FIXTURE(Configuration, "pkgs_dirs")
             {
                 std::string cache1 = util::path_concat(util::user_home_dir(), "foo");

commit 632d9a50a96ba39c689215590fb54b89c5313095

fix: Assign channel priority in `ctx.channels` order
Mirrored channels were processed in a separate first pass before regular
channels, causing them to always receive a higher priority regardless of
their position in the channels list. This broke strict channel priority
when a channel listed later (for example, conda-forge) had mirror
configuration while a channel listed earlier did not.

Process all channels in ctx.channels order and expand mirror
configuration at the configured channel's position. A mirrored channel
that is not listed in channels is also no longer loaded implicitly into
the solver.

The integration regression test resolves an overlapping package from an
earlier regular channel and a later mirrored channel under strict
priority. An additional unit test checks that mirror-only configuration
does not add repositories to the solver.

Bug introduced in 67f36d4a4815 (#3125).

Assisted-by: LLM

libmamba/src/api/channel_loader.cpp
@@ -242,37 +242,20 @@ namespace mamba
             int max_prio = static_cast<int>(ctx.channels.size());
             specs::CondaURL previous_channel_url;
 
-            // Process mirrored channels: create channel objects, configure mirrors, and initialize
-            // subdirs for each platform.
-            for (const auto& mirror : ctx.mirrored_channels)
-            {
-                for (const specs::Channel& channel :
-                     channel_context.make_channel(mirror.first, mirror.second))
-                {
-                    create_mirrors(channel, ctx.mirrors);
-                    create_subdirs(
-                        ctx,
-                        channel_context,
-                        channel,
-                        package_caches,
-                        subdirs,
-                        error_list,
-                        priorities,
-                        max_prio,
-                        previous_channel_url
-                    );
-                }
-            }
-
-            // Process regular (non-mirrored) channels.
+            // Process all channels in the order they appear in ctx.channels,
+            // expanding mirrored channels inline so that priority assignment
+            // follows the configured channel order. Previously, mirrored
+            // channels were processed in a separate first pass, causing them
+            // to always receive a higher priority than regular channels
+            // regardless of their position in the channels list.
             for (const auto& location : ctx.channels)
             {
-                if (ctx.mirrored_channels.contains(location))
-                {
-                    continue;
-                }
+                const auto mirror = ctx.mirrored_channels.find(location);
+                const auto& channels = (mirror != ctx.mirrored_channels.end())
+                                           ? channel_context.make_channel(mirror->first, mirror->second)
+                                           : channel_context.make_channel(location);
 
-                for (const specs::Channel& channel : channel_context.make_channel(location))
+                for (const specs::Channel& channel : channels)
                 {
                     if (channel.is_package())
                     {

libmamba/tests/src/core/test_channel_loader.cpp
@@ -185,6 +185,37 @@ TEST_CASE("load_channels", "[mamba::api][channel_loader]")
     REQUIRE(database.repo_count() == 0);
 }
 
+TEST_CASE("load_channels skips mirrored channels not in channels", "[mamba::api][channel_loader]")
+{
+    Context& ctx = mambatests::context();
+    mambatests::ScopedContextChange context_change{ ctx };
+    context_change.preserve(ctx.channels)
+        .preserve(ctx.mirrored_channels)
+        .preserve(ctx.pkgs_dirs)
+        .preserve(ctx.offline)
+        .preserve(ctx.remote_fetch_params)
+        .preserve(ctx.channel_alias);
+
+    ctx.channels = {};
+    ctx.mirrored_channels = { { "conda-forge", { "https://conda.anaconda.org/conda-forge" } } };
+    ctx.pkgs_dirs = {};
+    ctx.offline = true;
+
+    auto channel_context = ChannelContext::make_conda_compatible(ctx);
+    solver::libsolv::Database database(channel_context.params());
+    const auto tmp_dir = TemporaryDirectory();
+    auto package_caches = MultiPackageCache({ tmp_dir.path() }, ValidationParams{});
+
+    auto result = load_channels(ctx, channel_context, database, package_caches);
+
+    // Mirrored channels not explicitly listed in ctx.channels must not be
+    // loaded into the solver. Regression test for mirrored channels being
+    // processed in a separate first pass that assigned them higher priority
+    // than channels listed in ctx.channels.
+    REQUIRE(result.has_value());
+    REQUIRE(database.repo_count() == 0);
+}
+
 TEST_CASE("load_channels with root_packages", "[mamba::core][mamba::api::channel_loader]")
 {
     auto& ctx = mambatests::context();

micromamba/tests/test_create.py
@@ -2340,6 +2340,45 @@ def test_dummy_create(add_glibc_virtual_package, copy_channels_osx, tmp_home, tm
     res = helpers.create_with_chan_pkg(env_name, channels, package)
 
 
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_create_with_mirrored_channel_priority(
+    add_glibc_virtual_package, copy_channels_osx, tmp_home, tmp_root_prefix, tmp_path
+):
+    env_name = "mirrored-channel-priority"
+    channel_a = __this_dir__ / "channel_a"
+    channel_b = __this_dir__ / "channel_b"
+    rc_file = tmp_path / "config.yaml"
+    rc_file.write_text(
+        yaml.dump(
+            {
+                "channel_priority": "strict",
+                "channels": ["channel_a", "channel_b"],
+                "custom_channels": {
+                    "channel_a": str(channel_a),
+                    "channel_b": str(channel_b),
+                },
+                "mirrored_channels": {"channel_b": [channel_b.as_uri()]},
+            }
+        )
+    )
+
+    res = helpers.create(
+        "-n",
+        env_name,
+        "a",
+        "--dry-run",
+        "--json",
+        f"--rc-file={rc_file}",
+        default_channel=False,
+        no_rc=False,
+    )
+
+    links = [link for link in res["actions"]["LINK"] if link["name"] == "a"]
+    assert len(links) == 1
+    assert links[0]["version"] == "0.2.0"
+    assert "channel_a" in links[0]["channel"]
+
+
 @pytest.mark.parametrize("use_json", [True, False])
 def test_create_dry_run(tmp_home, tmp_root_prefix, use_json):
     env_name = "myenv"